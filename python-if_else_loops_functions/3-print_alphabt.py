8f30e3612567c4281c4dd4cecc04023a3fe4ec96
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-11 20:12:51 -0500

    Write a program that prints the ASCII alphabet, in lowercase

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alfbet in range(97, 123):
-    if((alfbet != 101) and (alfbet != 113)):
+    if ((alfbet != 101) and (alfbet != 113)):
         print("{}".format(chr(alfbet)), end='')
