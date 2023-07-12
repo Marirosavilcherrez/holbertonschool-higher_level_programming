994b1ce47eb1e0a7b43aa9de3fdb9ba7599462ba
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-12 14:52:31 -0500

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 9):
     for j in range(0, 10):
         if i != j and j > i:
             if (i != 8):
-                print("{}, ".format(str(i)+str(j)))
+                print("{}, ".format(str(i)+str(j)), end='')
             else:
-                print("{}".format(str(i)+str(j)), end='')
+                print("{}".format(str(i)+str(j))')
