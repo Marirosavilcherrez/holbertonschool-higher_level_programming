f41ad317302fd1cf6da2681fae887d1f70907c51
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-17 15:53:37 -0500

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     if (lenght == 0):
-        return(lenght, None)
+        return (lenght, None)
     else:
-        return(lenght, sentence[0])
+        return (lenght, sentence[0])
