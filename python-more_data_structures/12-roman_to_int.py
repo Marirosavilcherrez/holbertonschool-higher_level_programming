897898377cd00b77065eb0830b6e0fba7680ffd6
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-19 23:06:58 -0500

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
-        return(0)
+        return (0)
     roman = {
             "I": 1,
             "V": 5,
