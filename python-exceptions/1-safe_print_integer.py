2e5b9fbb64da3fb25d5bc2327f84bf1b619afb33
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-19 17:05:07 -0700

    Write a function that prints an integer with {:d}.format()

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,8 @@ def safe_print_integer(value):
         if value > 0 or value < 0:
             print("{:d}".format(value))
             return (True)
-    except IndexError:
+        if value == 0:
+            print("{:d}".format(value))
+            return (True)
+    except:
         return (False)
