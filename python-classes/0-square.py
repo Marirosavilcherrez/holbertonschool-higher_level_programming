de6298838cc55ff02fe1c7b929aeffdb4aaf0c0e
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-20 17:10:26 -0700

    Write an empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#python3 -c 'print(__import__("my_module").__doc__)'
-#python3 -c 'print(__import__("0-main.py").MyClass.__doc__)'
+"""Here goes the Square module"""
+
 class Square:
+"""Define a empty class"""
     pass
