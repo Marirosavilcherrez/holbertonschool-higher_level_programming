64c22939c61ab9a7c4ed46e80431070406885333
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-22 12:56:14 -0700

    Write a class Square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"A class Square that defines a square"
+"Private instance attribute size"
+
+
+class Square:
+    def __init__(self, size=0):
+        "Instantiation with size"
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise
+        except ValueError:
+            raise
