c0f687510749cfa5d42e33fa10529a7571260966
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-27 18:46:26 -0700

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry and public
+instance method"""
+
+
+class BaseGeometry:
+    """Simple BaseGeometry class"""
+    pass
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
