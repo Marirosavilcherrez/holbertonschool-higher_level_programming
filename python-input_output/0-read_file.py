d80320174fbc4a1f4580125d4a9e789d4e5ec0dd
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-29 22:32:17 -0700

    Write a function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+"""Function that reads a text file (UTF8)
+and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Function text file read only"""
+    with open(filename, "r", encoding="utf-8") as document:
+        text = document.read()
+    print(text, end="")
