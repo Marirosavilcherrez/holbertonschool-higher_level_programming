54bfa46bf55b19916dfa0d452c770c9c68270e74
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-31 13:58:34 -0500

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,19 +6,22 @@ the division matrix and div.
 
 
 def matrix_divided(matrix, div):
-    """Return the division of a matrix and a div,
-    return a float with 2 decimals."""
-    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """ Function divided module to div"""
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
         for i in row:
             if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of \
-                    integers/floats")
-    return [[round(item / div, 2) for item in row]for row in matrix]
+integers/floats")
+    return [[round(item / div, 2) for item in row] for row in matrix]
