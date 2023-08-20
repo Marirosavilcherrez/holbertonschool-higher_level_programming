309bd0b220a363f31fc7112437ee4a6c38609d68
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-20 00:22:38 -0500

    Update 101-avg_temperatures.sql

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- Script that import database from a table dump
 
-SELECT city, AVG(value) AS avg_temperature_fahrenheit
+SELECT city, AVG((temperature * 9/5) + 32) AS avg_temperature_fahrenheit
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temperature_fahrenheit DESC;
