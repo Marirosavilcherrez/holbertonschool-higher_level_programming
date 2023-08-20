cbfec39a57025d44fd82aae89d325ebdeb8b5075
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-19 23:43:42 -0500

    Update 100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,6 @@
 -- Script that converts hbtn_0c_0 database to UTF8 
 
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CREATE TABLE `first_table` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
