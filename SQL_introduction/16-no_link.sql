f4b2155fcc545a6a050f86649ef2f200c696598f
Marirosa <arqmvilcherrez@gmail.com>
2023-08-19 20:46:04 -0700

    script that list all record of table

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,8 +1,6 @@
 -- Script that lists all records of table second_table
 
-DELETE FROM second_table WHERE name = "George";
-INSERT INTO second_table (score, name) VALUES(18, "Aria");
-INSERT INTO second_table (score, name) VALUES(12, "Aria");
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL
+ORDER BY score DESC;
 
 
