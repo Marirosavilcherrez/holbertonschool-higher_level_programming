2e259d5efd12de37b2a05a3bebafd2bf2ff6de40
Marirosa <arqmvilcherrez@gmail.com>
2023-08-21 20:12:29 -0700

    Script that select cities from the state of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Script that creates the database hbtn_0d_usa and the table cities
+-- cities description: id INT unique, auto generated, NOT NULL and primary key
+
+SELECT id, name FROM cities WHERE state_id = 1;
