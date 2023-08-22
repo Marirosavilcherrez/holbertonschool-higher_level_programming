1d3fa37b35e5aab1c08a5d3350699a4011e801a4
Marirosa <arqmvilcherrez@gmail.com>
2023-08-22 15:22:51 -0700

    Script that use hbtn_0d_tvshows database The tv_shows table contains only one record where title = Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- Script that lists all contained in the database hbtn_0d_tvshows
+-- tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
