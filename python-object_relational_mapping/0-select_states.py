5264bf246ff66b67ed9a976c8a2e2fbbd4cfecd1
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-24 18:02:01 -0500

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ This is a Without ORM that lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 
-# Stablish conexion with the data base
+"Stablish conexion with the data base"
 conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -14,14 +14,14 @@ conn = MySQLdb.connect(
         db="hbtn_0e_0_usa",
         charset="utf8"
         )
-# Create a cursor to execute queries
+"Create a cursor to execute queries"
 cur = conn.cursor()
-# Execute a SELECT query and order by the "id" column
+"Execute a SELECT query and order by the "id" column"
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
-# Iterate through the results and display them
+"Iterate through the results and display them"
 for row in query_rows:
     print(row)
-# Close the cursor and the connection
+"Close the cursor and the connection"
 cur.close()
 conn.close()
