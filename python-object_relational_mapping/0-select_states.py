1923dfe8d8a202cbd345e7d461a9e1be6e401809
Marirosa <arqmvilcherrez@gmail.com>
2023-08-24 16:40:02 -0700

    Get all states from MySql in python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""This is a Without ORM that lists all states from the database hbtn_0e_0_usa"""
+"""Without ORM that lists all states from the database hbtn_0e_0_usa"""
 
+import sys
 import MySQLdb
 
-"Stablish conexion with the data base"
-conn = MySQLdb.connect(
+
+def main():
+    "Function that list all states from the database"
+    if len(sys.argv) == 4:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        mysql_database = sys.argv[3]
+
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user="root",
@@ -12,14 +20,14 @@ conn = MySQLdb.connect(
         db="hbtn_0e_0_usa",
         charset="utf8"
         )
-"Create a cursor to execute queries"
-cur = conn.cursor()
-"Execute a SELECT query and order by the "id" column"
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-"Iterate through the results and display them"
-for row in query_rows:
-    print(row)
-"Close the cursor and the connection"
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
