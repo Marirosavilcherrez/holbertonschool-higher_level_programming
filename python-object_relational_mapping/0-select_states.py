4d42349a5be977c0f83cf70b5cb736ae94b7ef6f
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-24 17:59:59 -0500

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# This is a Without ORM that lists all states from the database hbtn_0e_0_usa
+"""
+This is a Without ORM that lists all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 
 # Stablish conexion with the data base
