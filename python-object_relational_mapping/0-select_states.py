1ea69b3529f218345c93b936e11c9801ef58f3ac
Marirosa <arqmvilcherrez@gmail.com>
2023-08-24 15:56:50 -0700

    Correct pycodestyle in the code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-#This is a Without ORM that lists all states from the database hbtn_0e_0_usa
+# This is a Without ORM that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 
-#Stablish conexion with the data base
+# Stablish conexion with the data base
 conn = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user ="root",
-        passwd = "root",
-        db = "hbtn_0e_0_usa",
-        charset = "utf8"
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db="hbtn_0e_0_usa",
+        charset="utf8"
         )
-#Create a cursor to execute queries
+# Create a cursor to execute queries
 cur = conn.cursor()
-#Execute a SELECT query and order by the "id" column
+# Execute a SELECT query and order by the "id" column
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
 # Iterate through the results and display them
