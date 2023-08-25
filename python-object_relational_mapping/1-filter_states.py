ad7bc014109b2f7415ca55ce367d60871e0d8953
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-24 19:35:07 -0500

    Correct execute that display just N states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,11 +13,10 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8"
         )
     cur = conn.cursor()
     cur.execute(
-            "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+            "SELECT id, name FROM states WHERE name BINARY '[N]' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
