dd1de7614fb5eebf710b2692e8f6a651e7d697a3
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-08-25 13:52:00 -0500

    Add more comments to the code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+"Define the conection to database"
 db_user = "root"
 db_password = "root"
 db_host = "localhost"
@@ -12,12 +13,13 @@ db_port = 3306
 db_name = "hbtn_0e_6_usa"
 db_url = f"mysql+mysqldb://{db_user}:{db_password}@{db_host}:\
         {db_port}/{db_name}"
+"Create the database engine"
 engine = create_engine(db_url, pool_pre_ping=True)
 Base = declarative_base()
 
 
 class State(Base):
-    "Inherits from Base"
+    "Inherits from Base create a new table"
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
