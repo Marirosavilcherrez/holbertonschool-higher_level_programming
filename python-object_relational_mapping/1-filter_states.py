932efc0c358950238f6741df2f7a263ac874cc71
Marirosa <arqmvilcherrez@gmail.com>
2023-08-24 19:30:07 -0700

    Script should take 3 arguments: mysql username, mysql password and database name

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         )
     cur = conn.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+            "SELECT id, name FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
