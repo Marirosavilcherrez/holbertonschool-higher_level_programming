100cbb3597a5e267f1cc621849a6a602ff594378
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-15 16:56:58 -0700

    Script to fetch URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+//script that fetches the carachter name from URL
+
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+.then(response => response.json())
+.then(data => {
+    const characterName = data.name;
+    const characterElement = document.getElementById("character");
+    characterElement.textContent = characterName;
+})
+
+.catch(error => {
+  console.error('Error:', error);
+});
