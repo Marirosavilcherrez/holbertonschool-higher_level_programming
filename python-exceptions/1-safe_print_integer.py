ba0cfc01766043e9134ed977b9c53d025f7e7aeb
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-20 20:58:35 -0500

    Add files Python exception

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if value > 0 or value < 0:
+            print("{:d}".format(value))
+            return (True)
+        if value == 0:
+            print("{:d}".format(value))
+            return (True)
+    except Exception:
+        return (False)
