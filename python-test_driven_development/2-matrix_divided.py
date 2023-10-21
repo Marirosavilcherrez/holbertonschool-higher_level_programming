8fd511acfcfa7bbbf07b9aa976186aae922b6467
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-20 21:04:36 -0500

    Add files Python test driven

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is a module divided
+The module supply one function,
+the division matrix and div.
+"""
+
+
+def matrix_divided(matrix, div):
+    """ Function divided module to div"""
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) \
+            is not list or (len(matrix[0]) == 0):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in row:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    return [[round(item / div, 2) for item in row] for row in matrix]
