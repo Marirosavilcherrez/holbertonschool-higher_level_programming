dcdf7d87d3f0172065c993c8df25a445692ea1a0
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-20 20:28:55 -0500

    Delete 5-script.js

--- 5-script.js
+++
@@ -1,9 +0,0 @@
-/* Script add li element to list when click */
-
-$(document).ready(function() {
-    // Wait until document is ready
-
-    $("DIV#add_item").click(function() {
-        $("UL.my_list").append("<li>Item</li>"); //add to the list
-    });
-});
