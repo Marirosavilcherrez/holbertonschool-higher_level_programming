458ac3486920b58da2f55480b1f2666509b64abf
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-20 20:28:25 -0500

    Delete 3-script.js

--- 3-script.js
+++
@@ -1,8 +0,0 @@
-/* Add the class red to the header element when click */
-
-$(document).ready(function() {
-    // Wait until document is ready
-    $("DIV#red_header").click(function() {
-      $("header").addClass("red"); //add class
-    });
-  });
