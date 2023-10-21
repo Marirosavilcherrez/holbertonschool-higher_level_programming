94de0fcf829a7a8caa93215a4b4cb35b137bb089
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-20 20:27:51 -0500

    Delete 2-script.js

--- 2-script.js
+++
@@ -1,8 +0,0 @@
-/* Update the text color header when click */
-
-$(document).ready(function() {
-    // Wait until document is ready
-    $("DIV#red_header").click(function() {
-      $("header").css("color", "red");
-    });
-  });
